371a997e5cf4d3b456210ba29ef5e27cdccca4e8
nooraljohar <nooraljohar.a72@gmail.com>
2024-11-27 16:13:00 +0300

    hw

--- app.js
+++ app.js
@@ -1,37 +1,33 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-// Define the Customer schema
-const customerSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,  
-  },
-  age: {
-    type: Number,
-    required: true,  
-  }
-});
-
-
-const Customer = mongoose.model('Customer', customerSchema);
-
-module.exports = Customer;
+const Customer = require('./models/customers')
 
 // Connect to MongoDB using the URI from the .env file
 const connectDB = async () => {
     try {
-      const conn = await mongoose.connect(process.env.MONGODB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
-      console.log(`MongoDB connected: ${conn.connection.host}`);
+      const conn = await mongoose.connect(process.env.MONGODB_URI)   
+      console.log(`welcom to CRM`);
+
+    while(true){
+      console.log('What would you like to do?')
+      console.log('1. Create a customer')
+      console.log('2. View all customers')
+      console.log('3. Update a customer')
+      console.log('4. Delete a customer')
+      console.log('5. Quit')
+      const
+      if()
+      
+    }
     } catch (error) {
       console.error(`Error connecting to MongoDB: ${error.message}`);
       process.exit(1);  // Exit the process with an error code
     }
+
   };
   
-  module.exports = connectDB;
+  module.exports = connectDB
+  connectDB()
 
  
